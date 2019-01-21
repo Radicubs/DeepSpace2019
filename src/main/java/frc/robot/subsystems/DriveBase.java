7a84ffe302a132e7b785843f56c45b91b3520c14
CDBiddulph <caleb.d.biddulph@gmail.com>
2019-01-20 21:25:54 -0600

    Exponents easier, adjusts according to magnitude
    Made a method called adjustByExponent that allows rotational speed and forward speed to be individually adjusted by any exponent greater than zero.
    
    Also adjusted the values of rotational and forward speed such that holding the joystick all the way out in any direction will cause one motor to spin at full speed.

--- src/main/java/frc/robot/subsystems/DriveBase.java
+++ src/main/java/frc/robot/subsystems/DriveBase.java
@@ -20,24 +20,27 @@ public class DriveBase extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
     public void drive(double forwardSpeed, double rotationalSpeed) {
-
-        System.out.println(DifferentialDrive.kDefaultDeadband);
         
-        //This one line of code replaces all the code below it
-        //Additionally it is more accurate and accounts for input scaling at different speeds
-
-        double cubedFSpeed = -Math.pow(forwardSpeed, 3);
-        double cubedRSpeed = -Math.pow(rotationalSpeed, 3);
-
-        differentialDrive.arcadeDrive(cubedFSpeed, cubedRSpeed, false);
         
-        //This old version uses the default of squaring instead of cubing
-        //differentialDrive.arcadeDrive(forwardSpeed, rotationalSpeed, false);
+        //inverting these values make it work more intuitively
+        double adjustedFSpeed = -adjustByExponent(forwardSpeed, 3);
+        double adjustedRSpeed = -adjustByExponent(rotationalSpeed, .5);
 
+        //this magnitude assumes the range of the controller is a perfect circle
+        //it actually extends slightly beyond that, but it should be fine?
+        double magnitude = Math.sqrt(forwardSpeed * forwardSpeed + rotationalSpeed * rotationalSpeed);
+        double multiplier = magnitude / Math.max(Math.abs(adjustedFSpeed), Math.abs(adjustedRSpeed));
+
+        differentialDrive.arcadeDrive(multiplier * adjustedFSpeed, multiplier * adjustedRSpeed, false);
+        
         System.out.println("Raw Forward Speed: " + forwardSpeed);
         System.out.println("Raw Rotational Speed: " + rotationalSpeed);
+        System.out.println("Magnitude: " + magnitude);
         System.out.println("Left Motor Speed: " + LeftChassisMotor.get());
         System.out.println("Right Motor Speed: " + RightChassisMotor.get() + "\n");
+
+        //This old version uses the default of squaring instead of cubing
+        //differentialDrive.arcadeDrive(forwardSpeed, rotationalSpeed, false);
 
         // leftSpeed = forwardSpeed;
         // rightSpeed = forwardSpeed;
@@ -48,6 +51,12 @@ public class DriveBase extends Subsystem {
         // LeftChassisMotor.set(leftSpeed);
         // RightChassisMotor.set(-rightSpeed);
 
+    }
+
+    //takes the exponent of the positive value
+    //and copies the original sign
+    private static double adjustByExponent(double value, double exp) {
+        return Math.copySign(Math.pow(Math.abs(value), exp), value);
     }
 
     public DriveBase() {
